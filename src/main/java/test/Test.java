352df835c6a7d33ac5608aba3f360e98b70f99e4
xuezhongwei <email@example.com>
2019-07-13 22:49:22 +0800

    add test method

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		test();
 	}
-
+	public static void test() {
+		System.out.println("I am very happy tonight.");
+	}
 }
