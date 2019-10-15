783a5a5e7fd2a3766566b6e928f576351164d98c
xuezhongwei <email@example.com>
2019-10-15 08:25:27 +0800

    add code

---
+++ src/main/java/test/TestInterface.java
@@ -0,0 +1,16 @@
+package test;
+
+public interface TestInterface extends Interface1, Interface2 {
+	default void test() {
+		
+	}
+	
+}
+
+interface Interface1 {
+	
+}
+
+interface Interface2 {
+	
+}
