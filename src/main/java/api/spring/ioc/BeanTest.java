2ee7ffda47543b4ee2a0538b61984a13c2a85334
xuezhongwei <email@example.com>
2019-12-26 08:28:47 +0800

    add code

--- src/main/java/api/spring/ioc/BeanTest.java
+++ src/main/java/api/spring/ioc/BeanTest.java
@@ -1,3 +1,4 @@
+
 package api.spring.ioc;
 
 import org.springframework.context.ApplicationContext;
@@ -6,10 +7,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BeanTest {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("api/spring/service/beanDefinition.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("api/spring/ioc/beanDefinition.xml");
 		System.out.println("获取bean之前");
 		Person p = ctx.getBean(Person.class);
 		p.useAxe();
+		System.out.println(p.toString());
+		
+		DataSource ds = (DataSource)ctx.getBean("dataSource");
+		System.out.println(ds.toString());
 	}
 
 }
--- src/main/java/api/spring/ioc/Chinese.java
+++ src/main/java/api/spring/ioc/Chinese.java
@@ -1,7 +1,28 @@
 package api.spring.ioc;
 
-public class Chinese implements Person {
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Chinese implements Person, BeanNameAware, BeanFactoryAware, ApplicationContextAware,
+	InitializingBean, DisposableBean {
 	private Axe axe;
+	
+	private List<String> list;
+	
+	private Map<String, String> map;
+	
+	private Set<String> set;
+	
 	@Override
 	public void useAxe() {
 		System.out.println(axe.chop());
@@ -11,4 +32,67 @@ public class Chinese implements Person {
 		this.axe = axe;
 		System.out.println("正在调用setter方法");
 	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("execute destory method ");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("execute afterPropertiesSet method ");
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		System.out.println("execute setApplicationContext method ");
+	}
+
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		System.out.println("execute setBeanFactory method ");
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("execute setBeanName method ");
+	}
+	
+	public void myInit() {
+		System.out.println("execute myInit method ");
+	}
+	
+	public void myDestroy() {
+		System.out.println("execute myDestory method ");
+	}
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	public Map<String, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+
+	public Set<String> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<String> set) {
+		this.set = set;
+	}
+
+	@Override
+	public String toString() {
+		return "Chinese [axe=" + axe + ", list=" + list + ", map=" + map + ", set=" + set + "]";
+	}
+	
 }
