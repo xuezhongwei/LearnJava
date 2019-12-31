7adc2b3c75fae7827d56763650aa2c79cfe499e8
xuezhongwei <email@example.com>
2019-12-31 15:38:36 +0800

    add code

--- src/main/java/api/spring/database/JdbcTemplateDemo.java
+++ src/main/java/api/spring/database/JdbcTemplateDemo.java
@@ -1,5 +1,18 @@
 package api.spring.database;
 
+import javax.annotation.Resource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JdbcTemplateDemo {
+	@Resource
+	private JdbcTemplate jdbcTemplate;
 	
+	public void insert() {
+		String sql = "insert into user(name, age, telephone) values (?, ? , ?)";
+		Object[] args = new Object[] {"zhangsan", 90, "110"};
+		int count = jdbcTemplate.update(sql, args);
+	}
 }
--- src/main/java/api/spring/database/Main.java
+++ src/main/java/api/spring/database/Main.java
@@ -1,9 +1,14 @@
 package api.spring.database;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("api/spring/database/beanDefinition.xml");
+		JdbcTemplateDemo jdbcDemo = ctx.getBean(JdbcTemplateDemo.class);
+		
 	}
 
 }
