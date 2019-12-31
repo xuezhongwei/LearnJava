8b0254390c367d8282df8497afb88d967d308c0c
xuezhongwei <email@example.com>
2019-12-31 08:24:22 +0800

    add code

---
+++ src/main/java/api/spring/tx/TestDao.java
@@ -0,0 +1,21 @@
+package api.spring.tx;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestDao {
+	@Resource
+	private DataSource ds;
+	public void insert() throws InterruptedException {
+		JdbcTemplate jt = new JdbcTemplate(ds);
+		
+		jt.update("insert into customer(name,contact,telephone,email,remark) values('xzw','xzw','110','example@email.com','test')");
+		System.out.println("come here !");
+		Thread.sleep(5000);
+		throw new RuntimeException("test rollback");
+	}
+}
---
+++ src/main/java/api/spring/tx/TestTxMain.java
@@ -0,0 +1,14 @@
+package api.spring.tx;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestTxMain {
+
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("api/spring/tx/beanDefinition.xml");
+		TestDao tdao = ctx.getBean(TestDao.class);
+		tdao.insert();
+		System.out.println("over !");
+	}
+}
