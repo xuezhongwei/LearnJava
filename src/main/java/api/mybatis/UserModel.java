4f4ecf3be5733946ddc353951654f3250e3ffb65
xuezhongwei <email@example.com>
2020-01-16 08:51:27 +0800

    add code

---
+++ src/main/java/api/mybatis/UserModel.java
@@ -0,0 +1,38 @@
+package api.mybatis;
+
+public class UserModel {
+	int id;
+	String name;
+	int age;
+	String telephone;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	@Override
+	public String toString() {
+		return "UserModel [id=" + id + ", name=" + name + ", age=" + age + ", telephone=" + telephone + "]";
+	}
+	
+}
