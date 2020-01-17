520e3fd4b5b1db989416311a2ed0e2139e420c41
xuezhongwei <email@example.com>
2020-01-17 18:12:39 +0800

    add code

--- src/main/java/my/utils/GenerateCodeUtils.java
+++ src/main/java/my/utils/GenerateCodeUtils.java
@@ -31,7 +31,6 @@ public class GenerateCodeUtils {
 		try {
 			generateModel(packageName, tableName);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
