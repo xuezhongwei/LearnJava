135148e6c34990017bf44649d88a715df7047f2d
xuezhongwei <email@example.com>
2020-01-17 10:46:34 +0800

    add code

---
+++ src/main/java/api/mybatis/generateCode.java
@@ -0,0 +1,28 @@
+package api.mybatis;
+
+/**
+ * 生成mybatis相关代码
+ *
+ */
+public class generateCode {
+	/**
+	 * （1）生成与数据库表对应的model
+	 * （2）生成与数据库表mapper.xml
+	 * （3）生成与数据库表对应的dao
+	 * 
+	 * 因为不同企业对mybatis的使用会存在差异，所以这个实现会存在差异
+	 * 
+	 * 要考虑到表结构的更新
+	 */
+	
+	/**
+	 * 泽金版本
+	 */
+	public static void zejinVersion() {
+		
+	}
+	private static void generateModel(String packageName) {
+		
+	}
+	
+}
--- src/main/java/crazy/java/ThreadPoolDemo.java
+++ src/main/java/crazy/java/ThreadPoolDemo.java
@@ -3,7 +3,7 @@ package crazy.java;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class ThreadPoolTest {
+public class ThreadPoolDemo {
 
 	public static void  main(String[] args) {
 		ExecutorService pool = Executors.newFixedThreadPool(2);
