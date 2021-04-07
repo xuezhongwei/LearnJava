cb6703abe065cf420f5082aeeabae54fa23c79ab
xuezhongwei <email@example.com>
2021-04-07 17:24:29 +0800

    add code

--- src/main/java/my/utils/CheckUtils.java
+++ src/main/java/my/utils/CheckUtils.java
@@ -1,10 +1,12 @@
 package my.utils;
 
+import java.util.Date;
+
 public final class CheckUtils {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print(checkPercentStr("100.1"));
+		System.out.print(new Date());
 	}
 	/**
 	 * 校验金额字符串是否合法
@@ -17,6 +19,20 @@ public final class CheckUtils {
 			return false;
 		}
 	}
+	
+	/**
+	 * 校验金额字符串是否合法
+	 * 小数点后最多两位的正数
+	 */
+	public static boolean checkPercentStr(String moneyStr) {
+		if (null != moneyStr) {
+			return moneyStr.matches("(^(([1-9]{1}\\d?)|([0]{1}))(\\.(\\d){0,2})?$)|(100(\\.0{0,2})?)");
+			//return moneyStr.matches("100(\\.0{0,2})?");
+		} else {
+			return false;
+		}
+	}
+	
 	
 	/**
 	 * 校验金额字符串是否合法
